d071a5fa47f89989e992d7adc3ffad28981da821
Bernard Oosthuizen <bernard.oosthuizen@gmail.com>
2022-03-21 18:00:25 +0800

    About page updated

--- src/routes/Window.js
+++ src/routes/Window.js
@@ -31,7 +31,7 @@ const Window = () => {
                                           <img src={Banner} alt="ASCII Bernie Oosthuizen banner" /> 
                                    </div>
                                    <div> 
-                                          <h3>Computer Science Student</h3>
+                                          <h3>Web  |  Blockchain  |  Cloud</h3>
                                    </div>
                             </div>
                             {/* Pages are rendered to this <Outlet> */}
